c8b6f06c7d3c025dbb405a14439b8f6cce980c20
viktorzaharov <viktorzahar@gmail.com>
2023-02-18 13:36:04 +0300

    Version 0.0.2

--- lib/simple_dart_radio_field.dart
+++ lib/simple_dart_radio_field.dart
@@ -2,13 +2,13 @@ import 'dart:html';
 
 import 'package:simple_dart_ui_core/simple_dart_ui_core.dart';
 
-class RadioField extends PanelComponent
-    with ValueChangeEventSource<String>, MixinDisable
-    implements StateComponent<String> {
-  final List<RadioButtonInputElement> radioButtons = [];
+class RadioField<T> extends PanelComponent with ValueChangeEventSource<T?>, MixinDisable implements StateComponent<T?> {
+  final List<RadioButtonInputElement> radioButtons = <RadioButtonInputElement>[];
+  ObjectStringAdapter<T> adapter = (object) => object.toString();
   String _groupName = '';
+  final List<T> _optionList = <T>[];
 
-  RadioField() : super('RadioField') {
+  RadioField(this._groupName) : super('RadioField') {
     wrap = true;
   }
 
@@ -21,38 +21,71 @@ class RadioField extends PanelComponent
     });
   }
 
-  String get value => radioButtons.singleWhere((el) => el.checked ?? false).value ?? '';
+  T? get value {
+    assert(radioButtons.length == _optionList.length,
+        'radioButtons is not actual(${radioButtons.length} != ${_optionList.length})');
+    for (var i = 0; i < _optionList.length; i++) {
+      if (radioButtons[i].checked == true) {
+        return _optionList[i];
+      }
+    }
+    return null;
+  }
 
-  set value(String value) => radioButtons.singleWhere((el) => el.value == value).checked = true;
-
-  @override
-  String get state => value;
-
-  @override
-  set state(String newValue) => value = newValue;
-
-  void addRadioButton(String value, String text) {
-    final rowPanel = Panel()..vAlign = Align.center;
-    final radioButton = RadioButtonInputElement()
-      ..value = value
-      ..name = groupName;
-    final label = LabelElement()
-      ..style.paddingLeft = '3px'
-      ..style.paddingRight = '10px'
-      ..text = text
-      ..onClick.listen((e) {
-        if (disabled) {
-          return;
-        }
-        radioButton.checked = true;
+  set value(T? value) {
+    if (value == null) {
+      radioButtons.forEach((rb) {
+        rb.checked = false;
       });
-    radioButton.onChange.listen((ev) {
-      fireValueChange(radioButton.value!, value);
+      return;
+    }
+    for (var i = 0; i < _optionList.length; i++) {
+      if (_optionList[i] == value) {
+        radioButtons[i].checked = true;
+        return;
+      }
+    }
+  }
+
+  @override
+  String get state {
+    final val = value;
+    if (val == null) {
+      return '';
+    } else {
+      return adapter(val);
+    }
+  }
+
+  @override
+  set state(String newValue) => value = _optionList.firstWhere((element) => adapter(element) == newValue);
+
+  void initOptions(List<T> options) {
+    clear();
+    _optionList
+      ..clear()
+      ..addAll(options);
+    options.forEach((option) {
+      final rowPanel = Panel()..vAlign = Align.center;
+      final radioButton = RadioButtonInputElement()..name = groupName;
+      final label = LabelElement()
+        ..style.paddingLeft = '3px'
+        ..style.paddingRight = '10px'
+        ..text = adapter(option)
+        ..onClick.listen((e) {
+          if (disabled) {
+            return;
+          }
+          radioButton.checked = true;
+        });
+      radioButton.onChange.listen((ev) {
+        fireValueChange(value, value);
+      });
+      radioButtons.add(radioButton);
+      rowPanel.element.children.add(radioButton);
+      rowPanel.element.children.add(label);
+      add(rowPanel);
     });
-    radioButtons.add(radioButton);
-    rowPanel.element.children.add(radioButton);
-    rowPanel.element.children.add(label);
-    add(rowPanel);
   }
 
   void focus() {
